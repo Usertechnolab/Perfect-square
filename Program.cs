d1864f0f6ce5b29b8bf0c195a267c480bbe435b8
hp15f <hp15f@LAPTOP-1G0PTJV1>
2024-11-27 07:37:57 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿for (int number = 1; number <= 1000; number++)
+{
+    double sqrtResult = Math.Sqrt(number);
+    if (sqrtResult == (int)sqrtResult)
+    {
+        Console.WriteLine(number);
+    }
+}
